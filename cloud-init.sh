b21cbfe42bdd60da62fa651dcf986f9716252c2b
Simon Lauger <simon@lauger.name>
2018-04-12 17:17:03 +0200

    fixes for the cloud-init.sh script

--- cloud-init.sh
+++ cloud-init.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+(
+echo 'SYSTEM IS NOT READY - PLEASE WAIT UP TO 120 SECONDS'
+echo
+) > /etc/motd
+
 yum install -y epel-release
 yum install -y git ansible
 cd /root
@@ -8,4 +14,13 @@ cd /root
 ) > localhost.ini
 git clone https://github.com/slauger/ansible-training.git
 cd /root/ansible-training
-ansible-playbook -i /root/localhost.ini playbooks/setup.yml
+ansible-playbook -i /root/localhost.ini playbooks/setup.yml &> /root/cloud-init.log
+
+(
+echo ' _            _       _         '
+echo '| |_ __ __  _| | __ _| |__  ___ '
+echo '| | |_ \\ \/ / |/ _` | |_ \/ __|'
+echo '| | | | |>  <| | (_| | |_) \__ \'
+echo '|_|_| |_/_/\_\_|\__,_|_.__/|___/'
+echo
+) > /etc/motd
